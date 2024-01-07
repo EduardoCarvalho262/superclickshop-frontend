64bcf1a25873a57ecc23bdd52eaa97301d2accb1
EduardoCarvalho262 <eduardoc313@gmail.com>
2024-01-06 21:36:06 -0300

    feat: creating pages create account and forget password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { CreateAccountComponent } from './pages/login/create-account/create-account.component';
+import { ForgetPasswordComponent } from './pages/login/forget-password/forget-password.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent},
+  { path: 'create', component: CreateAccountComponent},
+  { path: 'forget', component: ForgetPasswordComponent},
   { path: 'home', component: HomePageComponent},
   { path: 'contato', component: ContactComponent},
 ];
